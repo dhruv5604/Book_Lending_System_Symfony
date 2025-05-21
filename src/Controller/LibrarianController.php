363ae05c445153e49ccfd08310c47962b10ae3a6
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-05-21 19:18:40 +0530

    added book add and edit feature

--- src/Controller/LibrarianController.php
+++ src/Controller/LibrarianController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookFormTypeForm;
 use App\Repository\BookRepository;
 use App\Repository\LoanRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\BookType;
+use App\Form\BookTypeForm;
 
 class LibrarianController extends AbstractController
 {   
@@ -29,5 +35,49 @@ class LibrarianController extends AbstractController
         ]);
     }
 
+    #[Route('add','app_add_book')]
+    public function addBook(Request $request, EntityManagerInterface $entityManager)
+    {
+        $book = new Book();
+        $form = $this->createForm(BookTypeForm::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('book/add.html.twig',[
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[Route('/edit/{id}',"app_edit_book")]
+    public function editBook(Book $book, Request $request, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(BookTypeForm::class,$book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('book/add.html.twig',[
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[Route('/delete/{id}', 'app_delete_book')]
+    public function delete(Book $book, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_dashboard');
+    }
 
 }
